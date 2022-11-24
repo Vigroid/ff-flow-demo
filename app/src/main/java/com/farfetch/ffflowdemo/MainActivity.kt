b14e8b5c632b12ae05b2ff3ca23caeb3461b7555
Vigo Yang <vigo.yang@farfetch.com>
2022-11-24 17:11:20 +0800

    Re-write Like bar with compose

--- app/src/main/java/com/farfetch/ffflowdemo/MainActivity.kt
+++ app/src/main/java/com/farfetch/ffflowdemo/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.farfetch.ffflowdemo
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,69 +22,34 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.farfetch.ffflowdemo.demo.ui.ProductList
 import com.farfetch.ffflowdemo.ui.theme.FarfetchFlowDemoTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     private val vm by lazy { MainViewModel() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val mainContent = findViewById<ComposeView>(R.id.compose_view)
-        mainContent.setContent {
+        setContent {
             FarfetchFlowDemoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    MainScreen(viewModel = vm) { isAdd, id ->
-                        vm.modifyLikeProducts(isAdd = isAdd, productId = id)
-                    }
+                    MainScreen(viewModel = vm)
                 }
             }
-        }
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                vm.likedTotalCount.collect { size ->
-                    updateViews(size)
-                }
-            }
-        }
-        // Clear all products to liked list
-        findViewById<Button>(R.id.btn_clear_all).setOnClickListener {
-            vm.clearLikedProducts()
-        }
-        // Add all products to liked list
-        findViewById<Button>(R.id.btn_add_all).setOnClickListener {
-            vm.likeAllProducts()
-        }
-        // Add all red products to liked list
-        findViewById<Button>(R.id.btn_add_red).setOnClickListener {
-            vm.likeRedProductsOnly()
-        }
-    }
-
-    // This function is called multiple times
-    // and the complexity of it will be increased if new features are added.
-    private fun updateViews(count: Int) {
-        val tvCount = findViewById<TextView>(R.id.tv_count)
-        tvCount.text = if (count == 0) {
-            getString(R.string.liked_items_empty)
-        } else {
-            getString(R.string.liked_items_text, count)
         }
     }
 }
 
 @Composable
-private fun MainScreen(viewModel: MainViewModel, onModifyLiked: (Boolean, String) -> Unit) {
+private fun MainScreen(viewModel: MainViewModel) {
     val uiState by viewModel.uiState.collectAsState()
     when {
         uiState.isLoading -> Box(
@@ -91,11 +64,72 @@ private fun MainScreen(viewModel: MainViewModel, onModifyLiked: (Boolean, String
         ) {
             Text(text = "Something went wrong")
         }
-        else -> ProductList(
-            modifier = Modifier.fillMaxSize(),
-            products = uiState.productList,
-            likedList = viewModel.likedProducts,
-            onModifyLiked = onModifyLiked
-        )
+        else -> Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            ProductList(
+                modifier = Modifier.weight(1f),
+                products = uiState.productList,
+                likedList = viewModel.likedProducts,
+                onModifyLiked = viewModel::modifyLikeProducts
+            )
+            val count by viewModel.likedTotalCount.collectAsState()
+            BottomBar(
+                totalCount = count,
+                onClickClear = viewModel::clearLikedProducts,
+                onClickLikeAll = viewModel::likeAllProducts,
+                onClickLikeRedOnly = viewModel::likeRedProductsOnly,
+            )
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    totalCount: Int,
+    onClickClear: () -> Unit,
+    onClickLikeAll: () -> Unit,
+    onClickLikeRedOnly: () -> Unit,
+) {
+    val displayText = if (totalCount == 0) {
+        stringResource(R.string.liked_items_empty)
+    } else {
+        stringResource(R.string.liked_items_text, totalCount)
+    }
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(48.dp)
+                .background(colorResource(id = R.color.teal_200))
+                .padding(horizontal = 6.dp),
+            horizontalArrangement = Arrangement.spacedBy(3.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = displayText,
+                modifier = Modifier
+                    .weight(1f),
+                fontSize = 20.sp
+            )
+            Button(contentPadding = PaddingValues(0.dp), onClick = onClickClear) {
+                Text(
+                    text = stringResource(id = R.string.clear_all_liked_items),
+                    fontSize = 12.sp
+                )
+            }
+            Button(contentPadding = PaddingValues(0.dp), onClick = onClickLikeAll) {
+                Text(
+                    text = stringResource(id = R.string.add_all_liked_items),
+                    fontSize = 12.sp
+                )
+            }
+            Button(contentPadding = PaddingValues(0.dp), onClick = onClickLikeRedOnly) {
+                Text(
+                    text = stringResource(id = R.string.like_red_items),
+                    fontSize = 12.sp
+                )
+            }
+        }
     }
 }
